b8abe5e7abf7543dd047201a50098c753b2ea3c7
JichouP <akkunn100@gmail.com>
2020-04-29 19:01:28 +0900

    fix bin

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
@@ -6,17 +7,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const argv_1 = __importDefault(require("argv"));
-exports.default = () => {
-    // define reg
-    const reg = [/ *\[[^\[\]]*\] */g, / *\([^\(\)]*\) */g];
-    // get args
-    const args = argv_1.default.run();
-    // rename
-    if (args.targets.length) {
-        for (const targetDir of args.targets) {
-            for (const name of fs_1.default.readdirSync(path_1.default.resolve(targetDir))) {
-                fs_1.default.renameSync(path_1.default.resolve(targetDir, name), path_1.default.resolve(targetDir, name.replace(reg[0], '').replace(reg[1], '')));
-            }
+// define reg
+const reg = [/ *\[[^\[\]]*\] */g, / *\([^\(\)]*\) */g];
+// get args
+const args = argv_1.default.run();
+// rename
+if (args.targets.length) {
+    for (const targetDir of args.targets) {
+        for (const name of fs_1.default.readdirSync(path_1.default.resolve(targetDir))) {
+            fs_1.default.renameSync(path_1.default.resolve(targetDir, name), path_1.default.resolve(targetDir, name.replace(reg[0], '').replace(reg[1], '')));
         }
     }
-};
+}
--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
+#!/usr/bin/env node
 import fs from 'fs';
 import path from 'path';
 import argv from 'argv';
 
-export default () => {
-    // define reg
-    const reg = [/ *\[[^\[\]]*\] */g, / *\([^\(\)]*\) */g];
+// define reg
+const reg = [/ *\[[^\[\]]*\] */g, / *\([^\(\)]*\) */g];
 
-    // get args
-    const args = argv.run();
+// get args
+const args = argv.run();
 
-    // rename
-    if (args.targets.length) {
-        for (const targetDir of args.targets) {
-            for (const name of fs.readdirSync(path.resolve(targetDir))) {
-                fs.renameSync(path.resolve(targetDir, name), path.resolve(targetDir, name.replace(reg[0], '').replace(reg[1], '')));
-            }
+// rename
+if (args.targets.length) {
+    for (const targetDir of args.targets) {
+        for (const name of fs.readdirSync(path.resolve(targetDir))) {
+            fs.renameSync(path.resolve(targetDir, name), path.resolve(targetDir, name.replace(reg[0], '').replace(reg[1], '')));
         }
     }
-};
+}
